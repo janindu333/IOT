26cc1addc5262671c6c193ef8508b33cabbc8cab
janindu <janindu.praneeth@gmail.com>
2024-06-25 13:04:31 +0530

    second commit of React app

---
+++ src/DeviceCard.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './DeviceCard.css'; // Import the CSS file
+
+const stateMapping = {
+  1: 'POWERON_STATE',
+  2: 'IDLE_STATE',
+  3: 'TAPCARD_STATE',
+  4: 'AUTHCARD_STATE',
+  5: 'AUTHPASS_STATE',
+  6: 'AUTHFAIL_STATE',
+  7: 'PRE_START_STATE',
+  8: 'START_STATE',
+  9: 'PRE_STOP_STATE',
+  10: 'STOP_STATE',
+  11: 'CLOSE_STATE'
+};
+
+const DeviceCard = ({ data }) => {
+  const currentState = stateMapping[data.currentState] || 'Unknown State';
+
+  return (
+    <div className="device-card">
+      <h2 className="device-header">Device ID: {data.deviceId}</h2>
+      <p className="device-info current-info"><span>Current:</span> {data.current} A</p>
+      <p className="device-info vc-info"><span>V_C Value:</span> {data.vcValue}</p>
+      <p className="device-info state-info"><span>Current State:</span> {currentState}</p>
+    </div>
+  );
+};
+
+export default DeviceCard;
---
+++ src/MQTTComponent.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import mqtt from 'mqtt';
+import DeviceCard from './DeviceCard';
+
+const MQTTComponent = () => {
+  const [devices, setDevices] = useState([]);
+
+  useEffect(() => {
+    const options = {
+      connectTimeout: 4000,
+      clientId: 'clientID_' + Math.random().toString(16).substr(2, 8),
+      username: 'janindu',
+      password: 'janindu',
+      keepalive: 60,
+      clean: true,
+    };
+
+    const client = mqtt.connect('ws://u5561ede.ala.dedicated.aws.emqxcloud.com:8083/mqtt', options);
+
+    client.on('connect', () => {
+      console.log('Connected to MQTT broker');
+      client.subscribe('test');
+    });
+
+    client.on('message', (topic, message) => {
+      const payload = JSON.parse(message.toString());
+      if (topic === 'test') {
+        const newDeviceData = {
+          deviceId: payload.deviceId,
+          current: payload.current,
+          vcValue: payload.vcValue,
+          currentState: payload.currentState
+        };
+        
+        setDevices(prevDevices => {
+          const index = prevDevices.findIndex(device => device.deviceId === newDeviceData.deviceId);
+          if (index > -1) {
+            const updatedDevices = [...prevDevices];
+            updatedDevices[index] = newDeviceData;
+            return updatedDevices;
+          } else {
+            return [...prevDevices, newDeviceData];
+          }
+        });
+      }
+    });
+
+    return () => {
+      if (client) {
+        client.end();
+      }
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>MQTT Devices</h1>
+      {devices.map(device => (
+        <DeviceCard key={device.deviceId} data={device} />
+      ))}
+    </div>
+  );
+};
+
+export default MQTTComponent;
